80434f3d8286a8d148de2d08a3c1df62f4252878
xumepyc-web <de.cheb21@gmail.com>
2023-03-07 20:29:28 +0300

    0.3.0

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,11 +1,16 @@
 import { Pagination } from 'antd';
 import { useState } from 'react';
+import './pagination.css';
 const Page = ({ onChangePage }) => {
   const [current, setCurrent] = useState(1);
   const onChange = (page) => {
     setCurrent(page);
     onChangePage(page);
   };
-  return <Pagination current={current} onChange={onChange} total={50} />;
+  return (
+    <div className="pagination-wrapper">
+      <Pagination className="pagination" current={current} onChange={onChange} total={50} />;
+    </div>
+  );
 };
 export default Page;
